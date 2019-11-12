47045cd81f9780442a643c0f43dcc4c98bd19be6
samirbhogayta <samir.bhogayta@gmail.com>
2019-11-12 12:20:44 +0530

    Update

---
+++ RepositoryPatternWithADO.Net/Repository/IEmployee.cs
@@ -0,0 +1,14 @@
+﻿using RepositoryPatternWithADO.Net.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPatternWithADO.Net.Repository
+{
+    interface IEmployee
+    {
+        IList<Employee> GetEmployees();        
+    }
+}
